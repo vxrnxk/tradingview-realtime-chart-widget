539305c620a281bbe359a83fe55beb3c44135793
Francisco V <vxrnxk@gmail.com>
2024-01-18 19:09:11 -0300

    version 1.0

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
+![JavaScript](https://img.shields.io/badge/javascript-%23323330.svg?style=for-the-badge&logo=javascript&logoColor=%23F7DF1E)
+
 # tradingview-realtime-chart-widget
-TradingView Advanced Real﻿-﻿Time Chart Widget
+TradingView Advanced Real-Time Chart Widget
+
+Real-time financial quotes using TradingView Widget
---
+++ main.js
@@ -0,0 +1,35 @@
+new TradingView.widget(
+    {
+        "autosize": true,
+        "symbol": "BMFBOVESPA:PETR4F",
+        "interval": "D",
+        "timezone": "Etc/UTC",
+        "theme": "dark",
+        "style": "1",
+        "locale": "en",
+        "enable_publishing": true,
+        "withdateranges": true,
+        "hide_side_toolbar": false,
+        "allow_symbol_change": true,
+        "watchlist": [
+            "BMFBOVESPA:TAEE4F",
+            "BMFBOVESPA:VALE3F",
+            "BMFBOVESPA:SAPR4F",
+            "BMFBOVESPA:BBAS3F",
+            "BMFBOVESPA:BBSE3F",
+            "BMFBOVESPA:CXSE3F",
+            "BMFBOVESPA:PETR4F"
+        ],
+        "details": true,
+        "hotlist": true,
+        "calendar": true,
+        "studies": [
+            "STD;EMA"
+        ],
+        "container_id": "chart",
+        "show_popup_button": true,
+        "popup_width": "1000",
+        "popup_height": "650",
+        "support_host": "https://www.tradingview.com"
+    }
+);
